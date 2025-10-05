148a6f9cc5fb1ff6ae94752e2d4f6739812ae50e
Saksham <sakshampatiyar26@gmail.com>
2025-10-05 17:48:33 +0530

    feat: little changes

--- Backend/server.js
+++ Backend/server.js
@@ -6,20 +6,9 @@ import NewsModel from "./models/news.js";
 import SignupModel from "./models/signup.js";
 
 const app = express();
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://news-application-olive.vercel.app/"
-];
-
+const allowedOrigins = ['https://news-application-olive.vercel.app'];
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
+  origin: allowedOrigins
 }));
 
 app.use(express.json());
